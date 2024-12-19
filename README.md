1a3721112a01c2c39233b32a05151d007d35e4be
yooooka <yooooka@gmail.com>
2024-12-19 15:21:58 -0800

    delete use client

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # SSSの画面遷移Prototype
+
 Stackは、Next.js 15 + React 19 with Tailwind, shadcn UI。
 v0なんかも駆使して時間をセーブしながらどこまで出来るかテスト。
----
+
 「パスワードをお忘れですか」からどうぞ。
--- src/components/PasswordReset.tsx
+++ src/components/PasswordReset.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
